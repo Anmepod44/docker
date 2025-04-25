0120cbcc0c54149859c7955ce56598631a965192
MARVIN-NEW <mnyaranga44@gmail.com>
2025-04-25 03:53:44 +0300

    initial-commit

---
+++ app.py
@@ -0,0 +1,13 @@
+# dependencies : Flask
+from flask import Flask
+
+app = Flask(__name__)
+
+# Define a route for the root URL
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    # Run the app in debug mode
+    app.run(debug=True)
