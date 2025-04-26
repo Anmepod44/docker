21f3c09c052a750f73a8e6677d801ba9b42da5e9
MARVIN-NEW <mnyaranga44@gmail.com>
2025-04-26 15:16:05 +0300

    saturdays image

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 # Define a route for the root URL
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World! This is the Flask app running in a Docker container.'
 
 if __name__ == '__main__':
     # Run the app in debug mode
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
