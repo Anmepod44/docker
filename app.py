057fc5c75d027fb7191cf8122f6ddbc879b6001e
MARVIN-NEW <mnyaranga44@gmail.com>
2025-05-02 03:09:36 +0300

    commented out import

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # dependencies: Flask, psycopg2
 from flask import Flask
-import psycopg2
 
 app = Flask(__name__)
 
@@ -15,20 +14,21 @@ DB_PASSWORD = 'mypassword'
 def hello_world():
     return 'Hello, World! This is the Flask app running in a Docker container. This is the second version Deji update!'
 
-@app.route('/db-status')
-def check_db_connection():
-    try:
-        conn = psycopg2.connect(
-            host=DB_HOST,
-            port=DB_PORT,
-            dbname=DB_NAME,
-            user=DB_USER,
-            password=DB_PASSWORD
-        )
-        conn.close()
-        return '✅ Successfully connected to the database!'
-    except Exception as e:
-        return f'❌ Failed to connect to the database: {e}'
+# @app.route('/db-status')
+# def check_db_connection():
+
+#     try:
+#         conn = psycopg2.connect(
+#             host=DB_HOST,
+#             port=DB_PORT,
+#             dbname=DB_NAME,
+#             user=DB_USER,
+#             password=DB_PASSWORD
+#         )
+#         conn.close()
+#         return '✅ Successfully connected to the database!'
+#     except Exception as e:
+#         return f'❌ Failed to connect to the database: {e}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
